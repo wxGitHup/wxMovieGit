e10780e4e0c8624a6fa27b6afc5cf005f105a047
wxSourcetree <718753218@qq.com>
2019-06-14 16:01:56 +0800

    添加模型

--- 维度电影/维度电影/电影/M/MovieModel.h
+++ 维度电影/维度电影/电影/M/MovieModel.h
@@ -7,19 +7,42 @@
 //
 
 #import <Foundation/Foundation.h>
-
 NS_ASSUME_NONNULL_BEGIN
 @class Details;
+@class Detail;
+@class Detai;
 @interface MovieModel : NSObject
 @property(nonatomic,strong)NSString * status;
 @property(nonatomic,strong)NSString*message;
 @property(nonatomic,strong)NSArray<Details*>*result;
-
+@property(nonatomic,strong)NSArray<Detail*>*resul;
+@property(nonatomic,strong)NSArray<Detai*>*resu;
 @end
 
 @interface  Details:NSObject
+@property(nonatomic,assign)BOOL followMovie;
+@property(nonatomic,assign)int id;
+@property(nonatomic,strong)NSString*imageUrl;
+@property(nonatomic,strong)NSString*name;
+@property(nonatomic,strong)NSString*summary;
+@end
 
+@interface  Detail:NSObject
+@property(nonatomic,assign)BOOL followMovie;
+@property(nonatomic,assign)int id;
+@property(nonatomic,strong)NSString*imageUrl;
+@property(nonatomic,strong)NSString*name;
+@property(nonatomic,strong)NSString*summary;
+@property(nonatomic,strong)NSString*releaseTimeShow;
+@end
 
+@interface  Detai:NSObject
+@property(nonatomic,assign)BOOL followMovie;
+@property(nonatomic,assign)int id;
+@property(nonatomic,strong)NSString*imageUrl;
+@property(nonatomic,strong)NSString*name;
+@property(nonatomic,strong)NSString*summary;
+@property(nonatomic,strong)NSString*releaseTimeShow;
 @end
 
 NS_ASSUME_NONNULL_END
