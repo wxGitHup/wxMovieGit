a5f43bb3367ce825291fe355801a2c7d377dd606
wxSourcetree <718753218@qq.com>
2019-06-14 10:26:24 +0800

    添加电影

---
+++ 维度电影/维度电影/电影/M/MovieModel.m
@@ -0,0 +1,13 @@
+//
+//  MovieModel.m
+//  维度电影
+//
+//  Created by 王翔 on 2019/6/14.
+//  Copyright © 2019年 Person. All rights reserved.
+//
+
+#import "MovieModel.h"
+
+@implementation MovieModel
+
+@end
